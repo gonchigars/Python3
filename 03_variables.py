bed9e42b9f6f6b05d02e668eada8e5add42cf895
VIDHYADHAR Kondaparthi <Vidhyadhar1215@gmail.com>
2022-08-25 11:58:16 +0530

    python4

--- 03_variables.py
+++ 03_variables.py
@@ -1,18 +1,18 @@
-print(f"20 days are {20*24*60} minutes")
-print(f"35 days are {35*24*60} minutes")
-print(f"45 days are {45*24*60} minutes")
-print(f"55 days are {55*24*60} minutes")
+print(f"20 days are {2*2*6} minutes")
+print(f"35 days are {3*2*6} minutes")
+print(f"45 days are {4*2*6} minutes")
+print(f"55 days are {5*2*6} minutes")
 
 # seconds
-print(f"20 days are {20*24*60*60} seconds")
-print(f"35 days are {35*24*60*60} seconds")
-print(f"45 days are {45*24*60*60} seconds")
-print(f"55 days are {55*24*60*60} seconds")
+print(f"20 days are {2*4*6*6} seconds")
+print(f"35 days are {3*2*6*6} seconds")
+print(f"45 days are {4*4*6*6} seconds")
+print(f"55 days are {5*2*6*6} seconds")
 print(f"-------------------------------")
 # variable
-calculate_to_units = 24 * 60
+calculate_to_units = 2 * 6
 name_of_unit = "minutes"
-print(f"20 days are {20*calculate_to_units} {name_of_unit}")
-print(f"35 days are {35*calculate_to_units} {name_of_unit}")
-print(f"45 days are {45*calculate_to_units} {name_of_unit}")
-print(f"55 days are {55*calculate_to_units} {name_of_unit}")
+print(f"2 days are {2*calculate_to_units} {name_of_unit}")
+print(f"5 days are {5*calculate_to_units} {name_of_unit}")
+print(f"4 days are {4*calculate_to_units} {name_of_unit}")
+print(f"5 days are {5*calculate_to_units} {name_of_unit}")
